c92c272825bdea9410b56de9094e5953e5800eba
Stefan-Alin Ursu <alinstefan.ursu@endava.com>
2021-11-10 09:39:36 +0200

    Homework 3 modifications

--- ai-tema3/src/main/java/Main.java
+++ ai-tema3/src/main/java/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 //        new Game(8, 1, 4).play();
         new GameMiniMax(8, 1, 4).play();
+//        new GameMiniMax(6, 1, 4).play();
     }
 }
